ad8abcec60bb87a95e9649cc2a18380dd724e4ee
Ride Prestige <faa1996a@gmail.com>
2026-06-19 15:58:28 +0100

    Use portal brand mark on public website

--- apps/web/src/components/layout/Header.tsx
+++ apps/web/src/components/layout/Header.tsx
@@ -22,7 +22,7 @@ export default function Header({ navigation, settings }: { navigation: Navigatio
 
           {/* Brand */}
           <Link href="/" className="shrink-0">
-            <BrandLogo width={90} src={settings.logoUrl} alt={settings.siteName} />
+            <BrandLogo width={90} alt={settings.siteName} />
           </Link>
 
           {/* Desktop nav */}
